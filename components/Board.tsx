e7f03bc3ce45559af9b2076c7e1a57ef4641f4b4
fre-ben <fschocke@gmail.com>
2021-02-16 11:37:18 +0100

    Refactor splice() to spread operator

--- components/Board.tsx
+++ components/Board.tsx
@@ -10,7 +10,7 @@ export default function Board() {
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
-    const newSquares = squares.slice();
+    const newSquares = [...squares];
     newSquares[i] = xIsNext ? "🦝" : "🦊";
     setSquares(newSquares);
     setXIsNext(!xIsNext);
